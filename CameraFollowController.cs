bf4c7d5e587b63baa3bc4840fe18b99dc8fb7f41
stupid-turtle <48131227+stupid-turtle@users.noreply.github.com>
2020-03-14 13:39:20 +0800

    Add files via upload

---
+++ CameraFollowController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowController : MonoBehaviour {
+
+    Transform follow;
+    string username;
+    // Start is called before the first frame update
+    void Start() {
+        username = PlayerPrefs.GetString("username", "default");
+        //Debug.Log(username);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void LateUpdate() {
+        follow = GameObject.Find(username).transform;
+        transform.position = new Vector3(follow.position.x, follow.position.y + 8f, follow.position.z - 10f);
+        transform.LookAt(follow);
+    }
+}
